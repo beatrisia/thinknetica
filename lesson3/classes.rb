93c290e0f1698cf2674fc257217c3632e0b442bf
beatrisia <nvelentsey@gmail.com>
2018-10-20 22:59:48 +0300

    1st commit lesson 3

--- lesson3/classes.rb
+++ lesson3/classes.rb
@@ -3,7 +3,15 @@ class Station
 
   def initialize(name)
     @name = name
+    @trains = []
   end
+
+  def add_train(train)
+    @trains << train
+  end
+
+  def delete_train(train)
+    @trains.delete(train)
 end
 
 class Route
